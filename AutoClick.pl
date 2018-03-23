74184a1f96284ad4960679708778f35e2d69b585
N3150DC-ITX\Nick-IB <1135066@gmail.com>
2018-03-24 04:09:38 +0900

    改良

--- AutoClick.pl
+++ AutoClick.pl
@@ -18,13 +18,14 @@ while ( defined(my $l = <$fh>) ) {
 }
 close $fh;
 
-# my $sel = Selenium::Remote::Driver->new( 'browser_name' => 'chrome' );
 my $sel = Selenium::Chrome->new();
 $sel->get('http://orteil.dashnet.org/cookieclicker/');
-$sel->execute_script("window.localStorage.setItem('CookieClickerGame', '$savedata')");
+$sel->execute_script("return window.localStorage.setItem('CookieClickerGame', '$savedata')");
 while (1) {
     last if ( $sel->execute_script("return window.Game.ready;") );
 }
+$sel->execute_script("window.Game.LoadSave();setInterval(window.Game.ClickCookie,20);window.Game.storeBulkButton(4)");
+$sel->execute_script("setInterval(()=>{window.Game.shimmers.forEach((shimmer) => shimmer.pop());window.Game.CloseNotes();window.Game.autoclickerDetected=0},500)");
 
 my $locator_golden_cookie = '//div[@class="shimmer"]';
 my $elem_big_cookie = $sel->find_element('//div[@id="bigCookie"]');
@@ -34,21 +35,14 @@ my $shouldUpgrade = 1;
 
 while ( 1 ) {
     try {
-        if ( &click($locator_golden_cookie, "goldenCookie") ) {
-            if ( $sel->execute_script("return window.Game.hasBuff('Cookie storm')") ) {
-                my $cnt = &click($locator_golden_cookie);
-                print "\nclick cookie storming($cnt)";
-            }
-
-            if ( $tm + 600 < time() ) {
-                $savedata = $sel->execute_script("return window.Game.WriteSave() || window.localStorage.getItem('CookieClickerGame')");
-                open my $fh, ">>", "savedata.txt" or die $!;
-                print $fh "$savedata\n";
-                close $fh;
-                $tm = time();
-                my ($sec, $min, $hour) = localtime($tm);
-                printf("\nsaved(%02d:%02d:%02d)", $hour, $min, $sec);
-            }
+        if ( $tm + 600 < time() ) {
+            $savedata = $sel->execute_script("return window.Game.WriteSave() || window.localStorage.getItem('CookieClickerGame')");
+            open my $fh, ">>", "savedata.txt" or die $!;
+            print $fh "$savedata\n";
+            close $fh;
+            $tm = time();
+            my ($sec, $min, $hour) = localtime($tm);
+            printf("\nsaved(%02d:%02d:%02d)", $hour, $min, $sec);
         }
         &buy_upgrade() if ( $shouldUpgrade );
         &buy_product();
@@ -89,18 +83,35 @@ sub click {
     my $target = shift;
     my $target_name = shift;
     my $cnt = 0;
+    while (1) {
+        my @elems;
+        my $loop;
+        try {
+            @elems = $sel->find_elements($target);
+        } catch {
+            @elems = ();
+        };
+        my $target_num = @elems;
 
-    while (scalar(my @elems = $sel->find_elements($target))) {
+        last if ( $target_num < 1 );
+        print "$target_num";
+        if ($target_num > 1) {
+            print "\ntry to click $target_num targets.";
+        }
+
+        $loop = 1;
         for my $elem (@elems) {
             try {
                 $elem->click();
                 ++$cnt;
             } catch {
-                last;
-            }
+                $loop = 0;
+            };
+            last if (!$loop);
         }
+        print "+";
     }
-    if ($cnt && $target_name) {
+    if ( $cnt && $target_name ) {
         print "\nclick $target_name(x$cnt)";
     }
     return $cnt;
